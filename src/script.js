3bd98634bfa658ef20971981806caa58eef17678
redflameken2 <202301178@iacademy.edu.ph>
2025-04-10 13:37:54 +0800

    email validation

--- src/script.js
+++ src/script.js
@@ -5,6 +5,19 @@ function validate_string(field_name) {
         value = input_field.value;
     return !(value === "");
 }
+function valid_email(email) {
+    if (email.indexOf('@') <= -1) {
+        return false;
+    }
+    return true;
+}
+function validate_email(field_name) {
+    var value = "";
+    var input_field = document.forms["registration_form"][field_name];
+    if (input_field != null)
+        value = input_field.value;
+    return valid_email(value);
+}
 function validate_checkbox(box_name) {
     var value = false;
     var checkbox = document.forms["registration_form"][box_name];
@@ -27,11 +40,11 @@ function write_warning(label_name, warning, valid) {
 function submit_event() {
     var first_name_valid = validate_string("first_name_field");
     var last_name_valid = validate_string("last_name_field");
-    var email_valid = validate_string("email_field");
+    var email_valid = validate_email("email_field");
     var agreed = validate_checkbox("agree_box");
     write_warning("first_name_label", "Do not leave empty", first_name_valid);
     write_warning("last_name_label", "Do not leave empty", last_name_valid);
-    write_warning("email_label", "Do not leave empty", email_valid);
+    write_warning("email_label", "Not a valid email", email_valid);
     write_warning("agree_label", "You must agree", agreed);
     if (first_name_valid && last_name_valid && email_valid && agreed) {
         submit_form("registration_form");
--- src/script.ts
+++ src/script.ts
@@ -6,6 +6,21 @@ function validate_string(field_name: string): boolean{
     return !(value === "");
 }
 
+function valid_email(email: string): boolean {
+    if(email.indexOf('@') <= -1){
+        return false;
+    }
+    return true;
+}
+
+function validate_email(field_name: string){
+    let value = "";
+    const input_field = document.forms["registration_form"][field_name];
+    if(input_field != null)
+        value = input_field.value;
+    return valid_email(value);
+}
+
 function validate_checkbox(box_name: string): boolean{
     let value = false;
     const checkbox = document.forms["registration_form"][box_name];
@@ -31,12 +46,12 @@ function write_warning(label_name: string, warning: string, valid: boolean){
 function submit_event(){
     let first_name_valid = validate_string("first_name_field");
     let last_name_valid = validate_string("last_name_field");
-    let email_valid = validate_string("email_field");
+    let email_valid = validate_email("email_field");
     let agreed = validate_checkbox("agree_box");
 
     write_warning("first_name_label", "Do not leave empty", first_name_valid);
     write_warning("last_name_label", "Do not leave empty", last_name_valid);
-    write_warning("email_label", "Do not leave empty", email_valid);
+    write_warning("email_label", "Not a valid email", email_valid);
     write_warning("agree_label", "You must agree", agreed);
 
     if(first_name_valid && last_name_valid && email_valid && agreed){
